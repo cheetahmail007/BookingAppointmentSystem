72e1c47dfdcc789dba01d36bd63aa4aec1b8ccc2
howdyev <phain118@outlook.com>
2023-02-07 15:03:50 -0500

    First push

--- app/src/main/java/com/example/appointmentbookingsystem/ui/RegisterDoctor.kt
+++ app/src/main/java/com/example/appointmentbookingsystem/ui/RegisterDoctor.kt
@@ -3,13 +3,67 @@ package com.example.appointmentbookingsystem.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.appointmentbookingsystem.R
+import com.example.appointmentbookingsystem.database.DbHelper
+import com.example.appointmentbookingsystem.database.dao.DoctorDao
+import com.example.appointmentbookingsystem.database.entity.Doctor
+import com.example.appointmentbookingsystem.databinding.ActivityRegisterDoctorBinding
 
+fun String.convertedInt(): Int {
+    if(this == "") return 0
+    var res = 0
+    try {
+        res = this.toInt()
+    } catch (e: java.lang.Exception) {
+    }
+    return res
+}
 //Add doctor in screen - Thomas
 class RegisterDoctor : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterDoctorBinding
+    private lateinit var dbHelper: DbHelper
+    private lateinit var doctorDao: DoctorDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_doctor)
+        binding = ActivityRegisterDoctorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initDatabase()
+        initViews()
+    }
 
-        // call add doctor
+    private fun initDatabase() {
+        dbHelper = DbHelper(this.applicationContext)
+        doctorDao = DoctorDao(dbHelper)
+    }
+
+    private fun initViews() {
+        binding.btnAdd.setOnClickListener {
+            val name = binding.editName.text.toString()
+            val mobileNo = binding.editMobileNo.text.toString()
+            val gender = binding.editGender.text.toString()
+            val specialization = binding.editSpecialization.text.toString()
+            val location = binding.editLocation.text.toString()
+            val rating = binding.editRating.text.toString()
+            val experience = binding.editExperience.text.toString()
+            val about = binding.editAbout.text.toString()
+            val email = binding.editEmail.text.toString()
+            doctorDao.addDoctor(
+                Doctor(
+                    "",
+                    name,
+                    mobileNo,
+                    gender,
+                    specialization,
+                    location,
+                    rating.convertedInt(),
+                    experience.convertedInt(),
+                    about,
+                    email)
+            )
+        }
+    }
+
+    fun registerDoctor() {
+
     }
 }
