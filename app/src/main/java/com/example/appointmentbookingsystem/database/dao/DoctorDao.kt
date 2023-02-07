3f6dc77491d155e3e8298a3689c1f2f923f3221a
1+1=2 <hau.chan@jessup.edu>
2023-02-07 12:25:49 -0800

    Revert "Merge branch 'master' into DashboardScreen"
    This reverts commit ce09bff85cabb574f8a155662f6f129e5716e320, reversing
    changes made to efedbe807bac067da7ecdcc9927bc8ec5e14e3e0.

--- app/src/main/java/com/example/appointmentbookingsystem/database/dao/DoctorDao.kt
+++ app/src/main/java/com/example/appointmentbookingsystem/database/dao/DoctorDao.kt
@@ -1,12 +1,10 @@
 package com.example.appointmentbookingsystem.database.dao
 
 import android.content.ContentValues
-import android.database.Cursor
 import com.example.appointmentbookingsystem.database.Constant
 import com.example.appointmentbookingsystem.database.Constant.DOCTOR_TABLE
 import com.example.appointmentbookingsystem.database.DbHelper
 import com.example.appointmentbookingsystem.database.entity.Doctor
-import com.example.appointmentbookingsystem.database.entity.Patient
 
 class DoctorDao(private val dbHelper: DbHelper) {
     private var database = dbHelper
@@ -28,34 +26,4 @@ class DoctorDao(private val dbHelper: DbHelper) {
         return database.writableDatabase.insert(DOCTOR_TABLE, null, contentValues)
     }
 
-    fun getAllDoctor(): ArrayList<Doctor> {
-        val doctorList = ArrayList<Doctor>()
-
-        val cursor: Cursor = database.readableDatabase.query("doctor", null, null, null, null, null, null)
-        while (cursor.moveToNext()) {
-            val doctorId = cursor.getLong(0)
-            val name = cursor.getString(1)
-            val mobileNo = cursor.getString(2)
-            val doctor = Doctor(doctorId,name,mobileNo,"","","",0,0,"0","")
-            doctorList.add(doctor)
-        }
-        return doctorList
-    }
-
-    fun deleteDoctor(patientId: Long): Boolean {
-        val numOfRowDeleted: Int = database.writableDatabase.delete("doctor", "doctorId=$patientId", null)
-        return numOfRowDeleted == 1
-    }
-
-    fun updateDoctor(patient: Patient): Boolean {
-        val contentValues = ContentValues()
-        contentValues.apply {
-            put("name", patient.name)
-            put("mobileNo", patient.mobileNo)
-        }
-
-        val numOfRowDeleted: Int =
-            database.writableDatabase.update("doctor", contentValues, "doctorId=${patient.id}", null)
-        return numOfRowDeleted == 1
-    }
 }
--- app/src/main/java/com/example/appointmentbookingsystem/database/entity/Doctor.kt
+++ app/src/main/java/com/example/appointmentbookingsystem/database/entity/Doctor.kt
@@ -1,7 +1,7 @@
 package com.example.appointmentbookingsystem.database.entity
 
 data class Doctor(
-    val id: Long,
+    val id: String,
     val name: String,
     val mobileNo: String,
     val gender: String,
